e5e9f0f2a130e008711e3998d0020440a99dc09a
Anuprash Gautam <anuprashgautam@hotmail.com>
2025-02-11 18:43:41 +0530

    Added the mechanism to upload the profile photo using nio package copy() method.

--- src/main/java/com/smart/controller/UserController.java
+++ src/main/java/com/smart/controller/UserController.java
@@ -1,14 +1,22 @@
 package com.smart.controller;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.security.Principal;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.smart.dao.UserRepository;
 import com.smart.entities.Contact;
@@ -54,21 +62,51 @@ public class UserController {
 	
 	// Processing add contact form.
 	@PostMapping("/process-contact")
-	public String processContact(@ModelAttribute Contact contact, Principal principal, HttpSession session) {
+	public String processContact(@ModelAttribute Contact contact,
+								Principal principal,
+								@RequestParam("profileImage") MultipartFile file,
+								HttpSession session) 
+	{
+		
+		boolean b=false;  // For storing the status.
+		try {
+			String name=principal.getName();
+			User user = this.userRepository.getUserByUserName(name);
+			
+			// Processing and uploading file(Image).
+			if(!file.isEmpty())
+			{
+				contact.setImage(file.getOriginalFilename());
 				
-		String name=principal.getName();
-		User user = this.userRepository.getUserByUserName(name);
-		
-		contact.setUser(user);					// Setting user in the contact.
-		user.getContacts().add(contact);		// Adding contact in the user's contact attribute.
-		
-		this.userRepository.save(user);
-		
-		// For add contact form.
-		session.setAttribute("message",new Message("Contact added Successfully!!!","alert-success"));
-		
-		// For the backend console.
-		System.out.println("Contact added successfully.");
+				File saveFile = new ClassPathResource("/static/img").getFile(); // The path where profileImage is going to be saved.
+				
+				Path path = Paths.get(saveFile.getAbsolutePath()+File.separator+file.getOriginalFilename());
+				
+				Files.copy(file.getInputStream(),path , StandardCopyOption.REPLACE_EXISTING);
+				
+				b=true;
+				System.out.println("Image uploaded successfully.");
+			}
+			
+			
+			contact.setUser(user);					// Setting user in the contact.
+			user.getContacts().add(contact);		// Adding contact in the user's contact attribute.
+			
+			
+			
+			this.userRepository.save(user);
+			
+			// For add contact form.
+			if(b) {
+				session.setAttribute("message",new Message("Contact added Successfully!!!","alert-success"));
+			}
+			
+			// For the back-end console.
+			System.out.println("Contact added successfully.");
+		} catch (Exception e) {
+			e.printStackTrace();
+			session.setAttribute("message",new Message("Something went wrong!!!","alert-danger"));
+		}
 		
 		return "normal/add_contact_form";
 	}
