a383db97e99c72014d168778cdd7cbb88c407477
Anuprash Gautam <anuprashgautam@hotmail.com>
2025-02-11 14:50:41 +0530

    Added the backend configurations to add the contact in the database and also showing the appropriate message on the add contact form.

--- src/main/java/com/smart/controller/UserController.java
+++ src/main/java/com/smart/controller/UserController.java
@@ -7,11 +7,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.smart.dao.UserRepository;
 import com.smart.entities.Contact;
 import com.smart.entities.User;
+import com.smart.helper.Message;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/user")
@@ -44,6 +48,27 @@ public class UserController {
 	public String openAddContactForm(Model model) {
 		model.addAttribute("title", "Add Contact");
 		model.addAttribute("contact",new Contact());
+		
+		return "normal/add_contact_form";
+	}
+	
+	// Processing add contact form.
+	@PostMapping("/process-contact")
+	public String processContact(@ModelAttribute Contact contact, Principal principal, HttpSession session) {
+				
+		String name=principal.getName();
+		User user = this.userRepository.getUserByUserName(name);
+		
+		contact.setUser(user);					// Setting user in the contact.
+		user.getContacts().add(contact);		// Adding contact in the user's contact attribute.
+		
+		this.userRepository.save(user);
+		
+		// For add contact form.
+		session.setAttribute("message",new Message("Contact added Successfully!!!","alert-success"));
+		
+		// For the backend console.
+		System.out.println("Contact added successfully.");
 		
 		return "normal/add_contact_form";
 	}
