a2ca16e036ace7c0c0f345da8385a78bd35b3fe5
Anuprash Gautam <anuprashgautam@hotmail.com>
2025-02-13 11:40:27 +0530

    Updated css.

--- src/main/java/com/smart/controller/UserController.java
+++ src/main/java/com/smart/controller/UserController.java
@@ -6,6 +6,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.security.Principal;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.smart.dao.ContactRepository;
 import com.smart.dao.UserRepository;
 import com.smart.entities.Contact;
 import com.smart.entities.User;
@@ -35,6 +37,9 @@ public class UserController {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private ContactRepository contactRepository;
 
 	
 	// Adding common data to response.
@@ -112,4 +117,23 @@ public class UserController {
 		
 		return "normal/add_contact_form";
 	}
+	
+	
+	// Show contacts handler
+	@GetMapping("/show-contacts")
+	public String showContacts(Model model, Principal principal) {
+		
+		model.addAttribute("title","Show User Contacts");
+		
+		String userName= principal.getName();
+		User user = this.userRepository.getUserByUserName(userName);
+		
+		// Fetching all the contacts of the logged in user.
+		List<Contact> contacts = this.contactRepository.findContactsByUser(user.getId());
+		
+		model.addAttribute("contacts",contacts);
+		
+		return "normal/show_contacts";
+	}
+	
 }
