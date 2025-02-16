266936593337dc48d3240a5cc99a26b2c6670d9e
Anuprash Gautam <anuprashgautam@hotmail.com>
2025-02-16 14:48:52 +0530

    Updating the new contact details with new profile photo.

--- src/main/java/com/smart/controller/UserController.java
+++ src/main/java/com/smart/controller/UserController.java
@@ -52,7 +52,7 @@ public class UserController {
 	public void addCommonData(Model model, Principal principal) {
 		String userName = principal.getName();
 
-		// Get the user using the username i.e. email.
+		// Get the user using the user-name i.e. email.
 		User user = userRepository.getUserByUserName(userName);
 
 		model.addAttribute("user", user);
@@ -210,11 +210,55 @@ public class UserController {
 	
 	// Handling the update contact form data and updating the respective contact.
 	@PostMapping("/process-update")
-	public String updateContact(Model model,HttpSession session) {
+	public String updateHandler(@ModelAttribute Contact contact,@RequestParam("profileImage") MultipartFile file,Model model,HttpSession session,Principal principle)
+	{
 		
-		session.setAttribute("message",new Message("Contact updated successfully!!!", "alert-success"));
+		try {
+			
+			//fetch old contact details
+			Contact oldcontactDetails=this.contactRepository.findById(contact.getcId()).get();
+			
+			
+			// Checking if the user want to update the photo or not.
+			if(!file.isEmpty())
+			{
+				//delete file
+				File deleteFile=new ClassPathResource("static/img").getFile();  // For path to that folder.
+				File file1=new File(deleteFile,oldcontactDetails.getImage());
+				file1.delete();
+				
+				//update image
+				File saveFile=new ClassPathResource("static/img").getFile();
+				
+				Path path=Paths.get(saveFile.getAbsolutePath()+File.separator+file.getOriginalFilename());
+				
+				Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+				contact.setImage(file.getOriginalFilename());
+			}
+			else
+			{
+				contact.setImage(oldcontactDetails.getImage());
+			}
+			
+			User user=this.userRepository.getUserByUserName(principle.getName());
+			
+			contact.setUser(user);
+			
+			this.contactRepository.save(contact);
+			
+			session.setAttribute("message", new Message("Contact updated successfully.","alert-success"));
+			
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+			session.setAttribute("message", new Message("Something went wrong.","alert-danger"));
+			
+		}
 		
-		return "normal/update_form";
+		
+		System.out.println("Conatct NAME="+contact.getName());
+		System.out.println("Contact ID="+contact.getcId());
+		return "redirect:/user/"+contact.getcId()+"/contact";
 	}
 }
 
